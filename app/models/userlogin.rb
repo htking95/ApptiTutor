59da4c88aa13a8d0cbef7b283411132cba70538c
Josh Stuckey <stuckey0@purdue.edu>
2015-12-02 13:08:09 -0500

    Fixed birthday searching issue
    make conditional for if birthday exists or not

--- app/models/userlogin.rb
+++ app/models/userlogin.rb
@@ -38,7 +38,13 @@ has_many :userreviews
   end
 
   def calculate_age(birthday)
+    if (birthday.blank?) then 
+      birthday = Time.now.utc.to_date
+      now = Time.now.utc.to_date
+      now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+    else
     now = Time.now.utc.to_date
     now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+    end
   end
 end
