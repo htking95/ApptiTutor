4efec2ddb8b49639868dd6450e2cd912cb70872c
cruz30 <cruz30@purdue.edu>
2015-12-05 15:29:34 -0500

    Profile Images on Search Results and Profile Page

--- app/models/userlogin.rb
+++ app/models/userlogin.rb
@@ -19,7 +19,7 @@ class Userlogin < ActiveRecord::Base
   ratyrate_rateable 'overall', 'clarity', 'knowledge', 'politeness', 'flexibility'
   ratyrate_rater
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#"}, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
   acts_as_messageable
