95a585a22a5eb04ace4c328bd5ecba95e11db9d8
cruz30 <cruz30@purdue.edu>
2015-12-07 14:18:34 -0500

    Advanced Search WIP

--- app/controllers/userlogins_controller.rb
+++ app/controllers/userlogins_controller.rb
@@ -12,7 +12,7 @@ class UserloginsController < ApplicationController
   	def search
   		if params[:search].present? || params[:price].present?
   			@userlogins = Userlogin.search params[:search], fields:[{aboutMe: :word_start}, {email: :word_start}, {classes: :word_start}, {skills: :word_start}]
-        @userlogins = Userlogin.where(price: params["price"]) if params[:price].present?
+        @userlogins = Userlogin.where("price <= ?", params["price"]) if params[:price].present?
   		else
   			@userlogins = Userlogin.all
   		end
